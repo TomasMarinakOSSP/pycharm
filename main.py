248e920ce1fdb1f914eea4e439bf8762b7e875a9
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-03 10:25:06 +0100

    2 routy v main.py

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return """
+        <h1>Hello World!</h1>
+        <p><a href="/">Domů</a></p>
+        <p><a href="/kontakt">Kontakt</a></p>
+    """
+
+
+@app.route("/kontakt")
+def kontakt():
+    return """
+        <h1>Toto je kontaktní stránka.</h1>
+        <p><a href="/">Domů</a></p>
+        <p><a href="/kontakt">Kontakt</a></p>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
