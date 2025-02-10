ad276cebb49953558b51ffc3c04f189211d30088
TomasMarinakOSSP <tomas.marinak@student.ossp.cz>
2025-02-10 09:27:33 +0100

    hotový odkazy commit

--- main.py
+++ main.py
@@ -11,24 +11,11 @@ def index():
 
 @app.route("/kontakt")
 def kontakt():
-    return """
-        <h1>Toto je kontaktní stránka.</h1>
-        <p><a href="/">Domů</a></p>
-        <p><a href="/kontakt">Kontakt</a></p>
-    """
+    return render_template("kontakt.html")
 
-
-@app.route("/<jmeno>")
-def hello(jmeno):
-    return f"Hello {jmeno}!"
-
-@app.route("/<cislo1>/<cislo2>")
-def dve_cisla(cislo1, cislo2):
-    try:
-        return f"Násobek je {int(cislo1) * int(cislo2)}"
-    except ValueError:
-        return "neplatné čísla"
-
+@app.route("/uzivatel")
+def user():
+    return render_template("uzivatel.html")
 
 
 if __name__ == "__main__":
